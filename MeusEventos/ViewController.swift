0de522f1087c0a4cb266c34ab1f0886c90f37f60
Claudio José <cjsm_menezes@yahoo.com.br>
2021-08-11 14:17:10 -0300

    Initial Commit

---
+++ MeusEventos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeusEventos
+//
+//  Created by CLAUDIO JOSÉ DA SILVA MENEZES on 11/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
